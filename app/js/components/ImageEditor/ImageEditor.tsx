eec171bcbfb283f843f9a5afd364a216e9df8bbd
Sepehr <sepehr.sy@gmail.com>
2022-02-05 22:37:34 +0330

    Moved JSON file generation logic to createJsonAndDownload util

--- app/js/components/ImageEditor/ImageEditor.tsx
+++ app/js/components/ImageEditor/ImageEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from 'react'
+import React, { useContext } from 'react'
 import {
     moveDown,
     moveLeft,
@@ -8,46 +8,19 @@ import {
     scaleUp,
 } from '../../store/canvasReducer'
 import { Store } from '../../store/Store'
-import pixelToInch from '../../utils/pixelToInch'
+import createJsonAndDownload from '../../utils/createJsonAndDownload'
 import Canvas from './Canvas'
 import './image-editor.scss'
 
 function ImageEditor(): JSX.Element {
+    const { state, dispatch } = useContext(Store)
     const {
-        state: {
-            width: canvasWidth,
-            height: canvasHeight,
-            image: { src, width, height, x, y, scale, ratio },
-            message,
-        },
-        dispatch,
-    } = useContext(Store)
+        image: { src },
+        message,
+    } = state
 
     const submitHandler = (): void => {
-        const data = {
-            canvas: {
-                width: pixelToInch(canvasWidth),
-                height: pixelToInch(canvasHeight),
-                photo: {
-                    src,
-                    width: pixelToInch(width),
-                    height: pixelToInch(height),
-                    x: pixelToInch(x),
-                    y: pixelToInch(y),
-                    scale: parseFloat(scale.toFixed(1)),
-                    ratio,
-                },
-            },
-        }
-        const json = JSON.stringify(data)
-        const blob = new Blob([json], { type: 'application/json' })
-        const href = URL.createObjectURL(blob)
-        const link = document.createElement('a')
-        link.href = href
-        link.download = `albelli-${(+new Date()).toString(36)}.json`
-        document.body.appendChild(link)
-        link.click()
-        document.body.removeChild(link)
+        createJsonAndDownload(state)
     }
 
     return (
---
+++ app/js/utils/createJsonAndDownload.ts
@@ -0,0 +1,34 @@
+import { CanvasState } from '../store/canvasReducer'
+import pixelToInch from './pixelToInch'
+
+export default (data: CanvasState): void => {
+    const {
+        width: canvasWidth,
+        height: canvasHeight,
+        image: { src, width, height, x, y, scale, ratio },
+    } = data
+    const cleanedData = {
+        canvas: {
+            width: pixelToInch(canvasWidth),
+            height: pixelToInch(canvasHeight),
+            photo: {
+                src,
+                width: pixelToInch(width),
+                height: pixelToInch(height),
+                x: pixelToInch(x),
+                y: pixelToInch(y),
+                scale: parseFloat(scale.toFixed(1)),
+                ratio,
+            },
+        },
+    }
+    const json = JSON.stringify(cleanedData)
+    const blob = new Blob([json], { type: 'application/json' })
+    const href = URL.createObjectURL(blob)
+    const link = document.createElement('a')
+    link.href = href
+    link.download = `albelli-${(+new Date()).toString(36)}.json`
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+}
